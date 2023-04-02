4dc9b70f88b2df9e7ca34f6e0c334229b29acaca
Yasser S <yasser.sissaoui@gmail.com>
2023-04-02 22:29:18 +0100

    added buttons and some stylin

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.container');
-const squareCount = 16;
+const squareCount = 100;
 
 for (let i = 0; i < squareCount * squareCount; i++) {
     const square = document.createElement('div');
@@ -7,16 +7,41 @@ for (let i = 0; i < squareCount * squareCount; i++) {
     container.appendChild(square);
 }
 
+let color = "black";
 const squares = document.querySelectorAll('.square');
+const colorButton = document.querySelector('.color');
+const eraseButton = document.querySelector('.eraser');
+const resetButton = document.querySelector('.reset');
+const colorPicker = document.querySelector(".picker");
+
+
+colorPicker.addEventListener("change", () => {
+    color = colorPicker.value;
+});
+
 squares.forEach(square => {
     square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'blue';
+        square.style.backgroundColor = color;
     });
 });
 
-const resetButton = document.querySelector('.reset');
+colorButton.addEventListener('click', () => {
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = color;
+        });
+    });
+});
+
 resetButton.addEventListener('click', () => {
     squares.forEach(square => {
-        square.style.backgroundColor = '#ccc';
+        square.style.backgroundColor = "white";
+    });
+});
+eraseButton.addEventListener('click', () => {
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "white";
+        });
     });
 });
