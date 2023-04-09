d618c5013af14f25d1b0c4cb94e578649d2406ce
Yasser S <yasser.sissaoui@gmail.com>
2023-04-09 02:55:26 +0100

    added silder, custom canvas size, and other stuff

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const container = document.querySelector('.container');
-const squareCount = 100;
+let container = document.querySelector('.container');
+let squareNum = 8;
 
-for (let i = 0; i < squareCount * squareCount; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
-}
+const root = document.documentElement;
+root.style.setProperty('--n', squareNum);
 
+let squares = genSquares(squareNum);
+
+
+const colors = ["#ff0000", "#ffa500", "#ffff00", "#008000", "#0000ff", "#4b0082", "#ee82ee"]
 let color = "black";
-const squares = document.querySelectorAll('.square');
+
+
 const colorButton = document.querySelector('.color');
 const eraseButton = document.querySelector('.eraser');
 const resetButton = document.querySelector('.reset');
+const rainbowButton = document.querySelector('.rainbow');
 const colorPicker = document.querySelector(".picker");
+const slider = document.querySelector(".slider");
 
-
-colorPicker.addEventListener("change", () => {
-    color = colorPicker.value;
-});
 
 squares.forEach(square => {
     square.addEventListener('mouseover', () => {
@@ -25,10 +25,33 @@ squares.forEach(square => {
     });
 });
 
+slider.addEventListener("input", () => {
+    squares = genSquares(Number(slider.value));
+    root.style.setProperty('--n', Number(slider.value));
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = color;
+        });
+    });
+});
+
+
+colorPicker.addEventListener("change", () => {
+    color = colorPicker.value;
+});
+
 colorButton.addEventListener('click', () => {
     squares.forEach(square => {
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = color;
+        });
+    });
+});
+
+rainbowButton.addEventListener('click', () => {
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = getRandomColor(colors);
         });
     });
 });
@@ -45,3 +68,17 @@ eraseButton.addEventListener('click', () => {
         });
     });
 });
+
+function getRandomColor(colors) {
+    return colors[Math.floor(Math.random() * 7)];
+}
+function genSquares(num) {
+    container.innerHTML = "";
+    for (let i = 0; i < num * num; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+    }
+    container = document.querySelector('.container');
+    return document.querySelectorAll('.square');
+}
