e6012f050ce07d3b3fff7c1d417d17801a6b7669
Yasser S <yasser.sissaoui@gmail.com>
2023-04-14 06:33:33 +0100

    fixed js,dark theme added

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-setFavicons();
-let container = document.querySelector(".container");
-let gridSize = 8;
-
 const root = document.documentElement;
-root.style.setProperty("--gridSize", gridSize);
-
-let grid = createGrid(gridSize);
+const colorBtn = document.querySelector(".color");
+const eraseBtn = document.querySelector(".eraser");
+const resetBtn = document.querySelector(".reset");
+const rainbowBtn = document.querySelector(".rainbow");
+const colorPicker = document.querySelector(".color-picker");
+const slider = document.querySelector(".slider");
 
 const rainbowColors = [
   "#ff0000",
@@ -16,27 +15,29 @@ const rainbowColors = [
   "#4b0082",
   "#ee82ee",
 ];
-let color = "black";
 
-const colorBtn = document.querySelector(".color");
-const eraseBtn = document.querySelector(".eraser");
-const resetBtn = document.querySelector(".reset");
-const rainbowBtn = document.querySelector(".rainbow");
-const colorPicker = document.querySelector(".color-picker");
-const slider = document.querySelector(".slider");
+let gridContainer = document.querySelector(".gridContainer");
+let gridSize = 8;
+let penColor = "black";
+
+root.style.setProperty("--gridSize", gridSize);
+let grid = createGrid(gridSize);
+
+setFavicons();
 
 colorPicker.addEventListener("input", () => {
-  setEventListeners(colorPicker.value);
-});
-
-colorBtn.addEventListener("click", () => {
-  setEventListeners(color);
+  penColor = colorPicker.value;
+  setEventListeners(penColor);
 });
 
 slider.addEventListener("change", () => {
   grid = createGrid(slider.value);
   root.style.setProperty("--gridSize", slider.value);
-  setEventListeners(color);
+  setEventListeners(penColor);
+});
+
+colorBtn.addEventListener("click", () => {
+  setEventListeners(penColor);
 });
 
 rainbowBtn.addEventListener("click", () => {
@@ -52,6 +53,7 @@ resetBtn.addEventListener("click", () => {
     cell.style.backgroundColor = null;
   });
 });
+
 eraseBtn.addEventListener("click", () => {
   setEventListeners(null);
 });
@@ -61,7 +63,6 @@ function setFavicons() {
   const favicon = document.createElement("link");
   favicon.setAttribute("rel", "icon");
   favicon.setAttribute("type", "image/x-icon");
-
   if (window.matchMedia("(prefers-color-scheme: dark)").matches === true) {
     favicon.setAttribute("href", "/assets/pen-light.svg");
   } else {
@@ -83,16 +84,16 @@ function getRainbowColor(rainbowColors) {
 }
 
 function createGrid(num) {
-  container.innerHTML = "";
+  gridContainer.innerHTML = "";
   for (let i = 0; i < num * num; i++) {
     const div = document.createElement("div");
     div.classList.add("cell");
     div.setAttribute("draggable", "false");
     div.addEventListener("mouseover", () => {
-      div.style.backgroundColor = color;
+      div.style.backgroundColor = penColor;
     });
-    container.appendChild(div);
+    gridContainer.appendChild(div);
   }
-  container = document.querySelector(".container");
+  gridContainer = document.querySelector(".gridContainer");
   return document.querySelectorAll(".cell");
 }
