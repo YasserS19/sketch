72f4fe8c6c5c779df78a94c6a69d22cc0b62a250
Yasser S <yasser.sissaoui@gmail.com>
2023-04-14 05:59:48 +0100

    fixed js, changed some css

--- script.js
+++ script.js
@@ -1,99 +1,98 @@
-const theme = window.matchMedia("(prefers-color-scheme: dark)");
-if (theme.matches === true) {
-    setFavicons("/assets/pen-light.svg")
-} else {
-    setFavicons("/assets/pen-dark.svg")
-}
-
-let container = document.querySelector('.container');
-let squareNum = 8;
+setFavicons();
+let container = document.querySelector(".container");
+let gridSize = 8;
 
 const root = document.documentElement;
-root.style.setProperty('--n', squareNum);
+root.style.setProperty("--gridSize", gridSize);
 
-let squares = genSquares(squareNum);
+let grid = createGrid(gridSize);
 
-
-const colors = ["#ff0000", "#ffa500", "#ffff00", "#008000", "#0000ff", "#4b0082", "#ee82ee"]
+const rainbowColors = [
+  "#ff0000",
+  "#ffa500",
+  "#ffff00",
+  "#008000",
+  "#0000ff",
+  "#4b0082",
+  "#ee82ee",
+];
 let color = "black";
 
-
-const colorButton = document.querySelector('.color');
-const eraseButton = document.querySelector('.eraser');
-const resetButton = document.querySelector('.reset');
-const rainbowButton = document.querySelector('.rainbow');
-const colorPicker = document.querySelector(".picker");
+const colorBtn = document.querySelector(".color");
+const eraseBtn = document.querySelector(".eraser");
+const resetBtn = document.querySelector(".reset");
+const rainbowBtn = document.querySelector(".rainbow");
+const colorPicker = document.querySelector(".color-picker");
 const slider = document.querySelector(".slider");
 
+colorPicker.addEventListener("input", () => {
+  setEventListeners(colorPicker.value);
+});
 
-squares.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = color;
+colorBtn.addEventListener("click", () => {
+  setEventListeners(color);
+});
+
+slider.addEventListener("change", () => {
+  grid = createGrid(slider.value);
+  root.style.setProperty("--gridSize", slider.value);
+  setEventListeners(color);
+});
+
+rainbowBtn.addEventListener("click", () => {
+  grid.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = getRainbowColor(rainbowColors);
     });
+  });
 });
 
-slider.addEventListener("input", () => {
-    squares = genSquares(Number(slider.value));
-    root.style.setProperty('--n', Number(slider.value));
-    squares.forEach(square => {
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = color;
-        });
-    });
+resetBtn.addEventListener("click", () => {
+  grid.forEach((cell) => {
+    cell.style.backgroundColor = null;
+  });
+});
+eraseBtn.addEventListener("click", () => {
+  setEventListeners(null);
 });
 
+function setFavicons() {
+  const head = document.querySelector("head");
+  const favicon = document.createElement("link");
+  favicon.setAttribute("rel", "icon");
+  favicon.setAttribute("type", "image/x-icon");
 
-colorPicker.addEventListener("change", () => {
-    color = colorPicker.value;
-});
-
-colorButton.addEventListener('click', () => {
-    squares.forEach(square => {
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = color;
-        });
-    });
-});
-
-rainbowButton.addEventListener('click', () => {
-    squares.forEach(square => {
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = getRandomColor(colors);
-        });
-    });
-});
-
-resetButton.addEventListener('click', () => {
-    squares.forEach(square => {
-        square.style.backgroundColor = "white";
-    });
-});
-eraseButton.addEventListener('click', () => {
-    squares.forEach(square => {
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = "white";
-        });
-    });
-});
-function setFavicons(path) {
-    const head = document.querySelector('head');
-    const favicon = document.createElement('link');
-    favicon.setAttribute('rel', 'icon');
-    favicon.setAttribute('type', 'image/x-icon');
-    favicon.setAttribute('href', path);
-    head.appendChild(favicon);
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches === true) {
+    favicon.setAttribute("href", "/assets/pen-light.svg");
+  } else {
+    favicon.setAttribute("href", "/assets/pen-dark.svg");
+  }
+  head.appendChild(favicon);
 }
 
-function getRandomColor(colors) {
-    return colors[Math.floor(Math.random() * 7)];
+function setEventListeners(color) {
+  grid.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = color;
+    });
+  });
 }
-function genSquares(num) {
-    container.innerHTML = "";
-    for (let i = 0; i < num * num; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        container.appendChild(square);
-    }
-    container = document.querySelector('.container');
-    return document.querySelectorAll('.square');
+
+function getRainbowColor(rainbowColors) {
+  return rainbowColors[Math.floor(Math.random() * 7)];
+}
+
+function createGrid(num) {
+  container.innerHTML = "";
+  for (let i = 0; i < num * num; i++) {
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    div.setAttribute("draggable", "false");
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = color;
+    });
+    container.appendChild(div);
+  }
+  container = document.querySelector(".container");
+  return document.querySelectorAll(".cell");
 }
